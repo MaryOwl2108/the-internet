7206b69a87be7b9bee7d249114e251b1c349ca39
Serikova_Mary <levdan42108@gmail.com>
2022-01-26 10:08:43 +0200

    solution

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,6 +17,7 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  import 'cypress-file-upload'
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
